2fdc8b831f2c929887b05fb14e75733c8b277bc0
Tendo33 <sjf1998112@gmail.com>
2025-03-25 20:19:40 +0800

    add demo

--- pydantic_ai/01_demo.py
+++ pydantic_ai/01_demo.py
@@ -2,19 +2,24 @@ import os
 
 from dotenv import load_dotenv
 from pydantic_ai import Agent
-from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.models.openai import OpenAIModel, OpenAIModelSettings
 from pydantic_ai.providers.openai import OpenAIProvider
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_API_BASE = os.getenv("OPENAI_API_BASE")
 
-model = OpenAIModel("gpt-4o", provider=OpenAIProvider(api_key=OPENAI_API_KEY,base_url=OPENAI_API_BASE))
+model = OpenAIModel(
+    "Hs-Deepseek-v3",
+    provider=OpenAIProvider(api_key=OPENAI_API_KEY, base_url=OPENAI_API_BASE),
+)
 
 agent = Agent(
     model=model,
     system_prompt="Be concise, reply with one sentence.",
 )
-
-result = agent.run_sync('Where does "hello world" come from?')
+setting = OpenAIModelSettings(temperature=0.8, top_p=0.95)
+result = agent.run_sync(
+    user_prompt='Where does "hello world" come from?', model_settings=setting
+)
 print(result.data)
