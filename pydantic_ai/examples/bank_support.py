c8f2d44b0230e8fda643bbbfd8bb149950bdaf86
Tendo33 <sjf1998112@gmail.com>
2025-03-28 14:38:35 +0800

    tiny fix

--- pydantic_ai/examples/bank_support.py
+++ pydantic_ai/examples/bank_support.py
@@ -94,14 +94,16 @@ async def customer_balance(
 
 if __name__ == "__main__":
     deps = SupportDependencies(customer_id=123, db=DatabaseConn())
-    result = support_agent.run_sync("What is my balance?", deps=deps)
+    result = support_agent.run_sync(
+        "What is my balance? 我有include_pending", deps=deps
+    )
     print(result.data)
-    """
-    support_advice='Hello John, your current account balance, including pending transactions, is $123.45.' block_card=False risk=1
-    """
+    print("=====================================================================")
+    print(result.all_messages())
+    print("=====================================================================")
 
     result = support_agent.run_sync("I just lost my card!", deps=deps)
     print(result.data)
-    """
-    support_advice="I'm sorry to hear that, John. We are temporarily blocking your card to prevent unauthorized transactions." block_card=True risk=8
-    """
+    print("=====================================================================")
+    print(result.all_messages())
+
